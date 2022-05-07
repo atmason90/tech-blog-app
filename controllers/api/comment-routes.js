3a378900d7a7c09ab7cb45c18e317e3a733eca33
atmason90 <atmason90@gmail.com>
2022-05-07 16:32:01 -0700

    edit render file in comment-routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -9,7 +9,7 @@ router.get('/', withAuth, async (req, res) => {
         });
         const comments = commentData.map((comment) => comment.get({ plain: true }));
         // console.log(comments);
-        res.render('post-comment', {comments, loggedIn: req.session.loggedIn});
+        res.render('single-post', {comments, loggedIn: req.session.loggedIn});
     } catch (err) {
         res.status(500).json(err);
     }
