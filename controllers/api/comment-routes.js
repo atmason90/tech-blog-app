ab4d4e736cd7372692f09ae9957cb08ca956619b
atmason90 <atmason90@gmail.com>
2022-05-08 13:38:18 -0700

    edit comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
-const { Comment } = require('../../models');
+const { Comment, User } = require('../../models/');
 const withAuth = require('../../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
     try {
         const commentData = await Comment.findAll({
-            include: [User],
+            include: User,
         });
         const comments = commentData.map((comment) => comment.get({ plain: true }));
-        // console.log(comments);
+        console.log(comments);
         res.render('single-post', {comments, loggedIn: req.session.loggedIn});
     } catch (err) {
         res.status(500).json(err);
@@ -16,10 +16,9 @@ router.get('/', withAuth, async (req, res) => {
 });
 
 router.post('/', withAuth, async (req, res) => {
-    const body = req.body;
     try {
         const newComment = await Comment.create({
-            ...body,
+            ...req.body,
             user_id: req.session.user_id,
         });
         res.json(newComment);
