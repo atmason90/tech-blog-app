99676a85112189df3719ca3557db4457428f6bf7
atmason90 <atmason90@gmail.com>
2022-05-09 15:15:34 -0700

    debug edit post functionality

--- public/js/edit.js
+++ public/js/edit.js
@@ -4,7 +4,7 @@ const postId = document.querySelector('input[name="post-id"]').value;
 const editFormHandler = async (event) => {
     event.preventDefault();
 
-    const post_title = document.querySelector('input[name="post-id"]').value;
+    const post_title = document.querySelector('input[name="post-title"]').value;
     const post_body = document.querySelector('textarea[name="post-body"]').value;
 
     // console.log(postTitle);
